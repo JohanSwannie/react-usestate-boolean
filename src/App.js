9e7c7da6038f1aeed5b7ceaa4de359e8d8d8d08a
jswan <jswannieman@gmail.com>
2023-07-18 12:44:19 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import React, { useState } from "react";
 
-const DisplayText = ({ text, maxLength }) => {
-  const [textHidden, setTextHidden] = useState(true);
+const DisplayText1 = ({ text1, maxLength }) => {
+  const [text1Hidden, setText1Hidden] = useState(true);
 
-  if (text.length <= maxLength) {
-    return <span>{text}</span>;
+  if (text1.length <= maxLength) {
+    return <span>{text1}</span>;
   }
   return (
     <div>
       <span>
-        {textHidden ? `${text.substr(0, maxLength).trim()}.......` : text}
+        {text1Hidden ? `${text1.substr(0, maxLength).trim()}.......` : text1}
         <br />
         <br />
-        {textHidden ? (
-          <button onClick={() => setTextHidden(false)}>read more</button>
+        {text1Hidden ? (
+          <button onClick={() => setText1Hidden(false)}>read more</button>
         ) : (
-          <button onClick={() => setTextHidden(true)}>read less</button>
+          <button onClick={() => setText1Hidden(true)}>read less</button>
         )}
       </span>
     </div>
   );
 };
 
+const DisplayText2 = ({ text2 }) => {
+  if (text2.includes("Pukeko")) {
+    return <span>"YES text2 definitely have the word Pukeko"</span>;
+  } else {
+    return <span>"No text2 DON'T have the word Pukeko"</span>;
+  }
+};
+
 const App = () => {
   return (
     <div className="App">
-      <DisplayText
-        text={`The real purpose of life is to spread love to others. If we can focus on that, the word will be a wonderful place. Let us all strife together to make this our aim.Indoing so we will achieve huge success in creating a better place on earth for everyone`}
+      <DisplayText1
+        text1={`The real purpose of life is to spread love to others. If we can focus on that, the word will be a wonderful place. Let us all strife together to make this our aim.Indoing so we will achieve huge success in creating a better place on earth for everyone`}
         maxLength={75}
       />
+      <br />
+      <DisplayText2 text2={`The Pukeka is swimming the the Lake`} />
     </div>
   );
 };
